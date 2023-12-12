53c245dbe037c9c0e82071894c9c4f2dfcc0a6c8
Daniel Schaffer <d.g.schaffer@gmail.com>
2023-12-12 16:30:59 +0100

    title changes on mousedown

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const myButton = document.querySelector("button");
 function changeColor(colors) {
   document.querySelector("body").style.backgroundColor = colors;
   document.querySelector("body").style.transition = "1s";
+  document.querySelector("title").innerHTML = "Good Night";
 }
